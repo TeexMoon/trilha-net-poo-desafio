03309148dcab1bf971e39a212314a699c00c9417
Everson Padilha Ferreira <eversonpadilhaf@hotmail.com>
2024-01-05 17:16:57 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Exemplo de uso
+        Nokia nokia = new Nokia("123456789", "Nokia 8", "IMEI123", 64);
+        Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
