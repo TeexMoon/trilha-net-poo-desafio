7e0c0deb67f801e576e45d653f20912d7c97ad8e
Everson Padilha Ferreira <eversonpadilhaf@hotmail.com>
2024-01-05 17:15:22 -0300

    Desafio concluído!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar, se necessário, alguma lógica específica para o iPhone
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+        }
     }
 }
