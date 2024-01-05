8c815903736e4d100ecb5e018ab26af61aa653c2
Everson Padilha Ferreira <eversonpadilhaf@hotmail.com>
2024-01-05 17:15:51 -0300

    Desafio concluído!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar, se necessário, alguma lógica específica para o Nokia
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Nokia...");
+        }
     }
 }
